4fdd0c89bba11e194726d0314a3a7b7b6d39fc51
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:29:41 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-custom-default-branch";
+metadata.url = "https://github.com/test-testperson/sample-custom-default-branch";
+end
